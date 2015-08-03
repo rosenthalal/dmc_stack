4f2063f9f6e4b8e86bc3ca7bc2153c8489530d5b
Alex <alex.e.rosenthal@gmail.com>
2015-08-03 13:03:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,32 +12,31 @@ def index():
 
 @app.route("/listings")
 def donorschoose_projects():
+	 #ORIENTDB IMPLEMENTATION
+	client = pyorient.OrientDB("localhost", 2424)
+	session_id = client.connect("root", "password")
 
-	# #ORIENTDB IMPLEMENTATION
-	# client = pyorient.OrientDB("localhost", 2424)
-	# session_id = client.connect("root", "password")
+	db_name = "property_test"
 
-	# db_name = "property_test"
+	if client.db_exists( db_name, pyorient.STORAGE_TYPE_MEMORY ):
+	 	client.db_open( db_name, "admin", "admin" )
+	else:
+	 	print "database does not exist!"
+	 	sys.exit()
 
-	# if client.db_exists( db_name, pyorient.STORAGE_TYPE_MEMORY ):
-	# 	client.db_open( db_name, "admin", "admin" )
-	# else:
-	# 	print "database does not exist!"
-	# 	sys.exit()
+	 recordsDict = {"type":"FeatureCollection","features":[]}
+	 records = client.command('SELECT FROM Listing WHERE [latitude,longitude,$spatial] NEAR [41.177407, 80.290192, {"maxDistance": 1}]')
 
-	# recordsDict = {"type":"FeatureCollection","features":[]}
-	# records = client.command('SELECT FROM Listing WHERE [latitude,longitude,$spatial] NEAR [41.177407, 80.290192, {"maxDistance": 1}]')
+	 for record in records:
+	 	recordDict = {"type":"Feature","properties":{},"geometry":{"type":"Point"}}
+	 	recordDict["id"] = record._rid
+	 	recordDict["properties"]["name"] = record.title
+	 	recordDict["properties"]["price"] = record.price
+	 	recordDict["geometry"]["coordinates"] = [record.longitude, record.latitude]
 
-	# for record in records:
-	# 	recordDict = {"type":"Feature","properties":{},"geometry":{"type":"Point"}}
-	# 	recordDict["id"] = record._rid
-	# 	recordDict["properties"]["name"] = record.title
-	# 	recordDict["properties"]["price"] = record.price
-	# 	recordDict["geometry"]["coordinates"] = [record.longitude, record.latitude]
+	 	recordsDict["features"].append(recordDict)
 
-	# 	recordsDict["features"].append(recordDict)
-
-	# client.db_close()
+	 client.db_close()
 
 
 	#DUMMY DATA IMPLEMENTATION
